725a3fdf5471ffebfc52da298f2a2f138b15fe99
Matthew J. Bryan <matthew.bryan@isilon.com>
2019-04-12 16:51:25 -0700

    Bump version for staging

--- version.py
+++ version.py
@@ -1,5 +1,5 @@
 __build__ = 0
-__version_info__ = (1, 0, 0, __build__)
+__version_info__ = (1, 0, 1, __build__)
 __version__ = "{0}.{1}.{2}.{3}".format(*__version_info__)
 
 COMBTEST_VERSION = __version__
